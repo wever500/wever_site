57e2f585e1ea01e22e9e787848ae34a25fc20c1d
wever500 <00001094269037sp@al.educacao.sp.gov.br>
2024-09-26 15:53:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('imcForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
+    const imc = (weight / (height * height)).toFixed(2);
+
+    let classification;
+    if (imc < 18.5) {
+        classification = 'Abaixo do peso';
+    } else if (imc < 24.9) {
+        classification = 'Peso normal';
+    } else if (imc < 29.9) {
+        classification = 'Sobrepeso';
+    } else {
+        classification = 'Obesidade';
+    }
+
+    document.getElementById('imcResult').innerHTML = `Seu IMC é ${imc} (${classification}).`;
+});
+
